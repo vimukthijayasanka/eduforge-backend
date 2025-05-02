09f08fe2ab106b004dbfffc644cf22d775ce87b5
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-02 20:57:50 +0530

    registera entity manager factory in root context

--- src/main/java/lk/ijse/dep13/eduforge/WebRootConfig.java
+++ src/main/java/lk/ijse/dep13/eduforge/WebRootConfig.java
@@ -1,7 +1,15 @@
 package lk.ijse.dep13.eduforge;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebRootConfig {
+
+    @Bean(destroyMethod = "close")
+    public EntityManagerFactory entityManagerFactory(){
+       return Persistence.createEntityManagerFactory("default");
+    }
 }
