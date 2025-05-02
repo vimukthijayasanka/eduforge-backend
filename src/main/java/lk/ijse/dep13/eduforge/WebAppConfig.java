f10b7f1367a19b49595e4d59c491683ccf30d297
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-02 14:21:33 +0530

    register multipart resolver

--- src/main/java/lk/ijse/dep13/eduforge/WebAppConfig.java
+++ src/main/java/lk/ijse/dep13/eduforge/WebAppConfig.java
@@ -1,11 +1,18 @@
 package lk.ijse.dep13.eduforge;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig {
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
