f605330182a3e7282027f14d5c0d5598ec5aa9ef
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-04 23:55:29 +0530

    Add CrudRepoImpl

--- src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/LecturerRepositoryImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/LecturerRepositoryImpl.java
@@ -1,56 +1,10 @@
 package lk.ijse.dep13.eduforge.repository.custom.impl;
 
-import jakarta.persistence.EntityManager;
 import lk.ijse.dep13.eduforge.entity.Lecturer;
-import lk.ijse.dep13.eduforge.entity.SuperEntity;
+import lk.ijse.dep13.eduforge.repository.CrudRepositoryImpl;
 import lk.ijse.dep13.eduforge.repository.custom.LecturerRepository;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.Optional;
+public class LecturerRepositoryImpl extends CrudRepositoryImpl<Lecturer, Integer> implements LecturerRepository {
 
-public class LecturerRepositoryImpl implements LecturerRepository {
 
-    private EntityManager entityManager;
-
-    @Override
-    public void setEntityManager(EntityManager entityManager) {
-            this.entityManager = entityManager;
-    }
-
-    @Override
-    public Lecturer save(Lecturer entity) {
-        entityManager.persist(entity);
-        return entity;
-    }
-
-    @Override
-    public void update(Lecturer entity) {
-        entityManager.merge(entity);
-    }
-
-    @Override
-    public void deleteById(Integer pk) {
-        entityManager.remove(entityManager.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public boolean existsById(Integer pk) {
-        return findById(pk).isPresent();
-    }
-
-    @Override
-    public Optional<Lecturer> findById(Integer pk) {
-        return Optional.ofNullable(entityManager.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public List<Lecturer> findAll() {
-        return entityManager.createQuery("SELECT l from Lecturer l",Lecturer.class).getResultList();
-    }
-
-    @Override
-    public long count() {
-        return entityManager.createQuery("SELECT COUNT(l) FROM Lecturer l", Long.class).getSingleResult();
-    }
 }
