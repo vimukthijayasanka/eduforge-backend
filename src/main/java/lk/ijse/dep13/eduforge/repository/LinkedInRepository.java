ceefaa8c8d59c34928cfd5ed0a2be2868fd850b9
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-04 18:20:30 +0530

    Add interface methods in each repos

--- src/main/java/lk/ijse/dep13/eduforge/repository/LinkedInRepository.java
+++ src/main/java/lk/ijse/dep13/eduforge/repository/LinkedInRepository.java
@@ -1,4 +1,23 @@
 package lk.ijse.dep13.eduforge.repository;
 
+import lk.ijse.dep13.eduforge.entity.Lecturer;
+import lk.ijse.dep13.eduforge.entity.LinkedIn;
+
+import java.util.List;
+
 public interface LinkedInRepository {
+
+    LinkedIn saveLinkedIn(LinkedIn linkedIn);
+
+    void updateLinkedIn(LinkedIn linkedIn);
+
+    void deleteLinkedInByLecturer(Lecturer lecturer);
+
+    void existsLinkedInByLecturer(Lecturer lecturer);
+
+    LinkedIn findLinkedInByLecturer(Lecturer lecturer);
+
+    List<LinkedIn> findAllLinkedIns();
+
+    long countLinkedIns();
 }
--- src/main/java/lk/ijse/dep13/eduforge/repository/PictureRepository.java
+++ src/main/java/lk/ijse/dep13/eduforge/repository/PictureRepository.java
@@ -1,4 +1,23 @@
 package lk.ijse.dep13.eduforge.repository;
 
+import lk.ijse.dep13.eduforge.entity.Lecturer;
+import lk.ijse.dep13.eduforge.entity.Picture;
+
+import java.util.List;
+
 public interface PictureRepository {
+
+    Picture savePicture(Picture picture);
+
+    void updatePicture(Picture picture);
+
+    void deletePictureByLecturer(Lecturer lecturer);
+
+    boolean existsPictureByLecturer(Lecturer lecturer);
+
+    Picture findPictureByLecturer(Lecturer lecturer);
+
+    List<Picture> findAllPictures();
+
+    long countPictures();
 }
