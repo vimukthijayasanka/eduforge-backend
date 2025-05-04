bc40329984e89a56e4d26cb597753647785e0708
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 02:16:27 +0530

    Finalize test cases of LecturerRepoImpl

--- src/test/java/lk/ijse/dep13/eduForge/repository/custom/impl/LecturerRepositoryImplTest.java
+++ src/test/java/lk/ijse/dep13/eduForge/repository/custom/impl/LecturerRepositoryImplTest.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LecturerRepositoryImplTest {
@@ -43,21 +46,64 @@ public class LecturerRepositoryImplTest {
 
     @Test
     void update(){
+        Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+        Lecturer savedLecturer = repository.save(lecturer);
+        savedLecturer.setName("Ellie Anderson");
+        savedLecturer.setDesignation("Assistant Professor");
+        savedLecturer.setQualification("Bsc (Hons) in SE");
+        savedLecturer.setType(LecturerType.VISITING);
+        repository.update(savedLecturer);
+
+        Lecturer actualLecturer = entityManager.find(Lecturer.class, savedLecturer.getId());
+        assertEquals(savedLecturer, actualLecturer);
     }
 
     @Test
     void deleteById(){
+        Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+        Lecturer savedLecturer = repository.save(lecturer);
+        repository.deleteById(savedLecturer.getId());
+
+        Lecturer dbLecturer = entityManager.find(Lecturer.class, savedLecturer.getId());
+        assertNull(dbLecturer);
     }
 
     @Test
-    void existsById(){}
+    void existsById(){
+        Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+        Lecturer savedLecturer = repository.save(lecturer);
+        boolean result = repository.existsById(savedLecturer.getId());
+        assertTrue(result);
+    }
 
     @Test
-    void findById(){}
+    void findById(){
+        Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+        Lecturer savedLecturer = repository.save(lecturer);
+        Optional<Lecturer> optionalLecturer1 = repository.findById(savedLecturer.getId());
+        Optional<Lecturer> optionalLecturer2 = repository.findById(-200);
+        assertTrue(optionalLecturer1.isPresent());
+        assertTrue(optionalLecturer2.isEmpty());
+    }
 
     @Test
-    void findAll(){}
+    void findAll(){
+        for (int i = 0; i < 8; i++) {
+            Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+            repository.save(lecturer);
+        }
+        List<Lecturer> allLecturers = repository.findAll();
+        assertEquals(8, allLecturers.size());
+        allLecturers.forEach(System.out::println);
+    }
 
     @Test
-    void count(){}
+    void count(){
+        for (int i = 0; i < 10; i++) {
+            Lecturer lecturer = new Lecturer("Joel Miller","Senior Lecturer","Bsc (Hons) in CS", LecturerType.FULL_TIME, 0);
+            repository.save(lecturer);
+        }
+        long count = repository.count();
+        assertEquals(10, count);
+    }
 }
