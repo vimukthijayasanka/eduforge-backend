dea49e089cc1b5e35e9ec81bc8a875e4f184e81f
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 14:11:06 +0530

    define LecturerService interface with core lecturer operations

--- src/main/java/lk/ijse/dep13/eduforge/service/custom/LecturerService.java
+++ src/main/java/lk/ijse/dep13/eduforge/service/custom/LecturerService.java
@@ -1,6 +1,24 @@
 package lk.ijse.dep13.eduforge.service.custom;
 
+import lk.ijse.dep13.eduforge.dto.request.LecturerReqTO;
+import lk.ijse.dep13.eduforge.dto.response.LecturerResTO;
+import lk.ijse.dep13.eduforge.entity.Lecturer;
 import lk.ijse.dep13.eduforge.service.SuperService;
+import lk.ijse.dep13.eduforge.util.LecturerType;
+
+import java.util.List;
 
 public interface LecturerService extends SuperService {
+    LecturerResTO saveLecturer(LecturerReqTO lecturerReqTO);
+
+    void updateLecturerDetailsWithImage(LecturerReqTO lecturerReqTO);
+
+    void updateLecturerDetailsWithoutImage(LecturerReqTO lecturerReqTO);
+
+    void deleteLecturer(Integer lecturerId);
+
+    LecturerResTO getLecturerDetails(Integer lecturerId);
+
+    List<LecturerResTO> getAllLecturers(LecturerType lecturerType);
+
 }
