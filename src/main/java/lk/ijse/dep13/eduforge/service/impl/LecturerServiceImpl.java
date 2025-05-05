c10b76ea7818dc8be99ee3244c588892f40e016c
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 20:30:40 +0530

    finalize save lecturer service

--- src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
@@ -1,7 +1,12 @@
 package lk.ijse.dep13.eduforge.service.impl;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
 import lk.ijse.dep13.eduforge.dto.request.LecturerReqTO;
 import lk.ijse.dep13.eduforge.dto.response.LecturerTO;
+import lk.ijse.dep13.eduforge.entity.Lecturer;
+import lk.ijse.dep13.eduforge.entity.LinkedIn;
+import lk.ijse.dep13.eduforge.entity.Picture;
 import lk.ijse.dep13.eduforge.repository.RepositoryFactory;
 import lk.ijse.dep13.eduforge.repository.custom.LecturerRepository;
 import lk.ijse.dep13.eduforge.repository.custom.LinkedInRepository;
@@ -9,8 +14,11 @@ import lk.ijse.dep13.eduforge.repository.custom.PictureRepository;
 import lk.ijse.dep13.eduforge.service.custom.LecturerService;
 import lk.ijse.dep13.eduforge.store.AppStore;
 import lk.ijse.dep13.eduforge.util.LecturerType;
+import lk.ijse.dep13.eduforge.util.Transformer;
 
+import javax.xml.transform.TransformerFactory;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LecturerServiceImpl implements LecturerService {
 
@@ -19,6 +27,8 @@ public class LecturerServiceImpl implements LecturerService {
     private final LinkedInRepository linkedInRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryTypes.LINKEDIN);
 
     private final PictureRepository pictureRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryTypes.PICTURE);
+
+    private final Transformer transformer = new Transformer();
 
     public LecturerServiceImpl() {
         lecturerRepository.setEntityManager(AppStore.getEntityManager());
@@ -30,8 +40,29 @@ public class LecturerServiceImpl implements LecturerService {
     public LecturerTO saveLecturer(LecturerReqTO lecturerReqTO) {
         AppStore.getEntityManager().getTransaction().begin();
         try{
+            Lecturer lecturer = transformer.fromLecturerReqTO(lecturerReqTO);
+            lecturer = lecturerRepository.save(lecturer);
+
+            if (lecturerReqTO.getLinkedin() != null) {
+                linkedInRepository.save(new LinkedIn(lecturer, lecturerReqTO.getLinkedin()));
+            }
+
+            String signUrl = null;
+            if (lecturerReqTO.getPicture() != null){
+                Picture picture = new Picture(lecturer, "lecturers/" + lecturer.getId());
+                pictureRepository.save(picture);
+
+                Blob blobId = AppStore.getBucket().create(picture.getPicturePath(),
+                        lecturerReqTO.getPicture().getInputStream(),
+                        lecturerReqTO.getPicture().getContentType());
+
+                signUrl = (blobId.signUrl(1, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature()).toString());
+            }
+
             AppStore.getEntityManager().getTransaction().commit();
-            return null;
+            LecturerTO lecturerTO = transformer.toLecturerTO(lecturer);
+            lecturerTO.setPicturePath(signUrl);
+            return lecturerTO;
         } catch (Exception e) {
             AppStore.getEntityManager().getTransaction().rollback();
             throw new RuntimeException(e);
