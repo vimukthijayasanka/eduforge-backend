437d5b95bd5f8769391275d8dbc8dfc35dd90427
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 11:49:30 +0530

    Add overrides methods in LinkedInRepoImpl

--- src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/LinkedInRepositoryImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/LinkedInRepositoryImpl.java
@@ -5,6 +5,16 @@ import lk.ijse.dep13.eduforge.entity.LinkedIn;
 import lk.ijse.dep13.eduforge.repository.CrudRepositoryImpl;
 import lk.ijse.dep13.eduforge.repository.custom.LinkedInRepository;
 
-public class LinkedInRepositoryImpl extends CrudRepositoryImpl<LinkedIn, Lecturer> implements LinkedInRepository{
+import java.util.Optional;
 
+public class LinkedInRepositoryImpl extends CrudRepositoryImpl<LinkedIn, Lecturer> implements LinkedInRepository{
+    @Override
+    public void deleteById(Lecturer pk) {
+        getEntityManager().remove(getEntityManager().find(LinkedIn.class, pk.getId()));
+    }
+
+    @Override
+    public Optional<LinkedIn> findById(Lecturer pk) {
+        return Optional.ofNullable(getEntityManager().find(LinkedIn.class, pk.getId()));
+    }
 }
