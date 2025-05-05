5681b008a161a994e61bcf81585ef03e1d4b8207
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 13:53:50 +0530

    Add generalize exception

---
+++ src/main/java/lk/ijse/dep13/eduforge/exception/AppException.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep13.eduforge.exception;
+
+public class AppException extends RuntimeException {
+    private final int errorCode;
+
+    public AppException(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public AppException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(String message, Throwable cause, int errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(Throwable cause, int errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+}
