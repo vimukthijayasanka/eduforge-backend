40f2f868330ed353f1e854e7e5e4fff8f7a1651e
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 11:54:59 +0530

    Update PictureRepoImpl

--- src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/PictureRepositoryImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/repository/custom/impl/PictureRepositoryImpl.java
@@ -5,5 +5,16 @@ import lk.ijse.dep13.eduforge.entity.Picture;
 import lk.ijse.dep13.eduforge.repository.CrudRepositoryImpl;
 import lk.ijse.dep13.eduforge.repository.custom.PictureRepository;
 
+import java.util.Optional;
+
 public class PictureRepositoryImpl extends CrudRepositoryImpl<Picture, Lecturer> implements PictureRepository {
+    @Override
+    public void deleteById(Lecturer pk) {
+        getEntityManager().remove(getEntityManager().find(Picture.class, pk.getId()));
+    }
+
+    @Override
+    public Optional<Picture> findById(Lecturer pk) {
+        return Optional.ofNullable(getEntityManager().find(Picture.class, pk.getId()));
+    }
 }
