3c05a70e1388ce2083ab0914b7e97cbd7bf8f1b4
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 14:19:01 +0530

    Fix the impl adding to LecturerServiceImpl after impl interface

--- src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
@@ -1,6 +1,40 @@
 package lk.ijse.dep13.eduforge.service.impl;
 
+import lk.ijse.dep13.eduforge.dto.request.LecturerReqTO;
+import lk.ijse.dep13.eduforge.dto.response.LecturerResTO;
 import lk.ijse.dep13.eduforge.service.custom.LecturerService;
+import lk.ijse.dep13.eduforge.util.LecturerType;
+
+import java.util.List;
 
 public class LecturerServiceImpl implements LecturerService {
+    @Override
+    public LecturerResTO saveLecturer(LecturerReqTO lecturerReqTO) {
+        return null;
+    }
+
+    @Override
+    public void updateLecturerDetailsWithImage(LecturerReqTO lecturerReqTO) {
+
+    }
+
+    @Override
+    public void updateLecturerDetailsWithoutImage(LecturerReqTO lecturerReqTO) {
+
+    }
+
+    @Override
+    public void deleteLecturer(Integer lecturerId) {
+
+    }
+
+    @Override
+    public LecturerResTO getLecturerDetails(Integer lecturerId) {
+        return null;
+    }
+
+    @Override
+    public List<LecturerResTO> getAllLecturers(LecturerType lecturerType) {
+        return List.of();
+    }
 }
