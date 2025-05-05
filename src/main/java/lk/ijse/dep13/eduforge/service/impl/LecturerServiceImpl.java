5e5413d9de3019c35a5aad732a0408b33cb7debf
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-05 15:46:08 +0530

    feat: add LecturerServiceImpl with transaction management setup

--- src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
+++ src/main/java/lk/ijse/dep13/eduforge/service/impl/LecturerServiceImpl.java
@@ -2,39 +2,96 @@ package lk.ijse.dep13.eduforge.service.impl;
 
 import lk.ijse.dep13.eduforge.dto.request.LecturerReqTO;
 import lk.ijse.dep13.eduforge.dto.response.LecturerResTO;
+import lk.ijse.dep13.eduforge.repository.RepositoryFactory;
+import lk.ijse.dep13.eduforge.repository.custom.LecturerRepository;
+import lk.ijse.dep13.eduforge.repository.custom.LinkedInRepository;
+import lk.ijse.dep13.eduforge.repository.custom.PictureRepository;
 import lk.ijse.dep13.eduforge.service.custom.LecturerService;
+import lk.ijse.dep13.eduforge.store.AppStore;
 import lk.ijse.dep13.eduforge.util.LecturerType;
 
 import java.util.List;
 
 public class LecturerServiceImpl implements LecturerService {
+
+    private final LecturerRepository lecturerRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryTypes.LECTURER);
+
+    private final LinkedInRepository linkedInRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryTypes.LINKEDIN);
+
+    private final PictureRepository pictureRepository = RepositoryFactory.getInstance().getRepository(RepositoryFactory.RepositoryTypes.PICTURE);
+
+    public LecturerServiceImpl() {
+        lecturerRepository.setEntityManager(AppStore.getEntityManager());
+        linkedInRepository.setEntityManager(AppStore.getEntityManager());
+        pictureRepository.setEntityManager(AppStore.getEntityManager());
+    }
+
     @Override
     public LecturerResTO saveLecturer(LecturerReqTO lecturerReqTO) {
-        return null;
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+            return null;
+        } catch (Exception e) {
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updateLecturerDetailsWithImage(LecturerReqTO lecturerReqTO) {
-
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+        }catch (Exception e){
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updateLecturerDetailsWithoutImage(LecturerReqTO lecturerReqTO) {
-
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+        }catch (Exception e){
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteLecturer(Integer lecturerId) {
-
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+        }catch (Exception e){
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public LecturerResTO getLecturerDetails(Integer lecturerId) {
-        return null;
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+            return null;
+        }catch (Exception e){
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<LecturerResTO> getAllLecturers(LecturerType lecturerType) {
-        return List.of();
+        AppStore.getEntityManager().getTransaction().begin();
+        try{
+            AppStore.getEntityManager().getTransaction().commit();
+            return List.of();
+        }catch (Exception e){
+            AppStore.getEntityManager().getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
     }
 }
